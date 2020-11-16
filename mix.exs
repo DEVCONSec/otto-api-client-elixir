ca950df65f1be5b658f1aa84e8700621b286f209
Josh Summitt <jsummitt@devcondetect.com>
2020-11-16 16:01:32 -0600

    Upgraded my local version of elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OttoApi.MixProject do
     [
       app: :otto_api,
       version: "0.1.0",
-      elixir: "~> 1.9.1",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
