79468e988581e69df2fa84d0a55ea74e134dc5d5
jfukura <jfukura@gmail.com>
2020-12-01 07:33:49 -0800

    Changing the Alert Data Structure to what is currently in the project.

--- lib/otto_api/alert.ex
+++ lib/otto_api/alert.ex
@@ -5,7 +5,7 @@ defmodule OttoApi.Alert do
   alias OttoApi.Client
 
   @spec all(client :: %Client{}, account_id :: binary, site_id :: binary) ::
-          {:ok, list(%__MODULE__{id: binary, site_id: binary, alert_type_id: binary, description: binary, inserted_at: binary})}
+          {:ok, list(%__MODULE__{id: binary,  site_id: site_id, alert_type: %{ id: binary, name: binary, description: binary, severity: binary}, summary: binary, detail: %{ description: binary, origin: binary, destination: binary }, alert_count: binary})}
 
   def all(client, account_id, site_id) do
     {:ok,
@@ -15,9 +15,24 @@ defmodule OttoApi.Alert do
 
     alerts =
       Enum.map(records, fn record ->
-        %{"id" => id, "site_id" => site_id, "alert_type_id" => alert_type_id, "description" => description, "inserted_at" => inserted_at} = record
+        %{"id" => id,
+          "site_id" => site_id,
+          "alert_type" => %{
+            "id" => alert_type_id,
+            "name" => alert_type_name,
+            "description" => alert_type_description,
+            "severity" => alert_type_severity
+          },
+          "summary" => alert_summary,
+          "detail" => %{
+            "description" => description,
+            "origin" => detail_origin,
+            "destination" => detail_destination
+          },
+          "alert_count"=> alert_count
+        } = record
 
-        %__MODULE__{id: id,  site_id: site_id, alert_type_id: alert_type_id, description: description, inserted_at: inserted_at}
+        %__MODULE__{id: id,  site_id: site_id, alert_type: %{ id: alert_type_id, name: alert_type_name, description: alert_type_description, severity: alert_type_severity}, summary: alert_summary, detail: %{ description: description, origin: detail_origin, destination: detail_destination }, alert_count: alert_count}
       end)
 
     {:ok, alerts}
