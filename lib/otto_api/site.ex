7ae45f8b3a457ec32ef55a2b3d940e31366c1de5
Chad Fowler <chad@chadfowler.com>
2020-12-10 13:06:39 -0600

    site doesn't have a name

--- lib/otto_api/site.ex
+++ lib/otto_api/site.ex
@@ -1,5 +1,5 @@
 defmodule OttoApi.Site do
-  @enforce_keys [:url, :account_id, :name]
+  @enforce_keys [:url, :account_id]
   defstruct @enforce_keys ++ [:id, :inserted_at]
 
   alias OttoApi.Client
@@ -9,7 +9,6 @@ defmodule OttoApi.Site do
            list(%__MODULE__{
              id: binary,
              url: binary,
-             name: binary,
              account_id: binary,
              inserted_at: binary
            })}
@@ -24,7 +23,6 @@ defmodule OttoApi.Site do
         %{
           "id" => id,
           "url" => url,
-          "name" => name,
           "account_id" => account_id,
           "inserted_at" => inserted_at
         } = record
@@ -32,7 +30,6 @@ defmodule OttoApi.Site do
         %__MODULE__{
           id: id,
           url: url,
-          name: name,
           account_id: account_id,
           inserted_at: inserted_at
         }
@@ -49,7 +46,6 @@ defmodule OttoApi.Site do
      %__MODULE__{
        id: site_attributes["id"],
        url: site_attributes["url"],
-       name: site_attributes["name"],
        account_id: account_id,
        inserted_at: site_attributes["inserted_at"]
      }}
--- test/site_test.exs
+++ test/site_test.exs
@@ -6,7 +6,7 @@ defmodule Site do
 
   test "can get all sites for account" do
     stub_json = """
-    {"data":[{"id":"c9572706-36e5-48c2-86be-7429ae4c3bae", "name": "DEVCON", "url":"https://example.com", "account_id":"an account id", "inserted_at":"when"}]}
+    {"data":[{"id":"c9572706-36e5-48c2-86be-7429ae4c3bae", "url":"https://example.com", "account_id":"an account id", "inserted_at":"when"}]}
     """
 
     api = OttoApi.Client.new("jwt", "http://example.com/api/v2")
@@ -20,7 +20,6 @@ defmodule Site do
                 %OttoApi.Site{
                   id: "c9572706-36e5-48c2-86be-7429ae4c3bae",
                   url: "https://example.com",
-                  name: "DEVCON",
                   account_id: "an account id",
                   inserted_at: "when"
                 }
@@ -30,8 +29,7 @@ defmodule Site do
   test "can create site from struct" do
     site = %{
       url: "https://example.com",
-      account_id: "account's id",
-      name: "DEVCON"
+      account_id: "account's id"
     }
 
     request_body = Jason.encode!(site)
@@ -40,7 +38,6 @@ defmodule Site do
     response_body = """
     {"data":{"id":"123456",
               "url":"https://example.com",
-              "name" : "DEVCON",
               "account_id":"account's id",
               "inserted_at": "when"
               }
@@ -56,7 +53,6 @@ defmodule Site do
              {:ok,
               %OttoApi.Site{
                 url: "https://example.com",
-                name: "DEVCON",
                 account_id: "account's id",
                 id: "123456",
                 inserted_at: "when"
