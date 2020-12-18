e6184dd78ef86b4b6467537e5613205ea941462c
Chad Fowler <chad@chadfowler.com>
2020-12-18 10:28:44 -0600

    reverting

--- lib/otto_api/account.ex
+++ lib/otto_api/account.ex
@@ -25,7 +25,7 @@ defmodule OttoApi.Account do
   end
 
   def create(client, account_attributes) do
-    {:ok, account_attributes} = Client.post(client, "/accounts", %{"account" => account_attributes})
+    {:ok, %{"data" => account_attributes}}= Client.post(client, "/accounts", %{"account" => account_attributes})
 
     {:ok,
      %__MODULE__{
