d09b8bb29dadca0203a15c9629a28656bf210fc3
Josh Summitt <josh@otto-js.com>
2022-04-06 15:00:53 -0500

    fixing bug in test

--- test/invitation_test.exs
+++ test/invitation_test.exs
@@ -6,7 +6,7 @@ defmodule InvitationTest do
 
   test "can accpet invitation" do
     stub_json = """
-    {"data":{"secret_key":"c9572706-36e5-48c2-86be-7429ae4c3bae", 
+    {"data":{"secret_key":"c9572706-36e5-48c2-86be-7429ae4c3bae",
               "account_id":"aee969dd-f91a-4cb4-ac35-b69e1716f25b",
               "account_name":"test name",
               "inviter_id":"f8c7e5b6-1064-4c22-ae9e-17f94af4bd33",
@@ -27,10 +27,10 @@ defmodule InvitationTest do
     OttoApi.Http.MockClient
     |> expect(:put, fn _url, request_body, _headers, _options -> {:ok, %{status_code: 200, body: stub_json}} end)
 
-    assert OttoApi.Invitation.accept(api, "aee969dd-f91a-4cb4-ac35-b69e1716f25b", "c9572706-36e5-48c2-86be-7429ae4c3bae") ==
+    assert OttoApi.Invitation.accept(api, "c9572706-36e5-48c2-86be-7429ae4c3bae") ==
              {:ok,
                 %OttoApi.Invitation{
-                secret_key: "c9572706-36e5-48c2-86be-7429ae4c3bae", 
+                secret_key: "c9572706-36e5-48c2-86be-7429ae4c3bae",
                 account_id: "aee969dd-f91a-4cb4-ac35-b69e1716f25b",
                 account_name: "test name",
                 inviter_id: "f8c7e5b6-1064-4c22-ae9e-17f94af4bd33",
